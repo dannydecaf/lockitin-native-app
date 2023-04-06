ec03a4b376cee47cbf1187598655864927dbd478
Dan Coffey <dcoffey90@gmail.com>
2023-04-06 16:59:07 +0100

    Re-added firebase.js from old repo

---
+++ firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAE6L0LbQgpzZI2qDC5AjWWKBcy7sFSFAk",
+  authDomain: "lockitin-native.firebaseapp.com",
+  projectId: "lockitin-native",
+  storageBucket: "lockitin-native.appspot.com",
+  messagingSenderId: "367382106570",
+  appId: "1:367382106570:web:ffb04ae02186ab0cb1ff33"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore()
+
+export { auth, db }
--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,18 +1,10 @@
-import { View, Text } from "react-native";
 import React, { createContext, useContext } from "react";
-import * as Google from "expo-google-app-auth";
 
 const AuthContext = createContext({
      
 })
 
 export const AuthProvider = ({ children }) => {
-
-   const signInWithGoogle = async() => {
-    await Google.logInAsync()
-   } 
-
-
   return (
     <AuthContext.Provider value={{
 
